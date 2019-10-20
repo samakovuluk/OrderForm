2701e03c6c5ddfc66a1071a7fb9b8ba729877263
Ulukbek Samakov <uluk.samakov@gmail.com>
2019-10-20 18:53:00 +0300

    learning components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Info from "./Components/Info";
+class App extends React.Component{
+    render() {
+        return(
+            <div>
+                <div>
+                    Hello
+                </div>
+                <Info>
+                </Info>
+            </div>
+        );
+    }
+}
+export default App;
